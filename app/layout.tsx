fb3196a6546518464e9fbd2c1f432c47afabbfaf
Pranay Chatterjee <pranay.chatterjee29@gmail.com>
2026-02-09 23:17:43 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,10 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        
         <meta name="google-site-verification" content="yLPddTsRuwmxFCf8A82MB-iolThMZ4Gx7auvSZzsRVM" />
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-46GCTXG4D4"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
